ab35a98ae063dc5f17f875e28c6e7eb79b6dea6f
amourrxniah <nraprice1@gmail.com>
2025-05-28 17:31:32 +0100

    c

--- Weather_App/src/components/LandingPage.jsx
+++ Weather_App/src/components/LandingPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LandingPage = ({ onStart }) => {
     return (
         <div className="landing">
-            <img src="./public/weather.png" alt="Weather Icon" className="landing-icon" />
+            <img src="/weather.png" className="landing-icon" alt="Weather icon" />
             <h1 className="landing-title">Welcome to Weatherly</h1>
             <p className="landing-subtitle">Your modern weather companion</p>
             <button className="start-button" onClick={onStart}>Get Started</button>
